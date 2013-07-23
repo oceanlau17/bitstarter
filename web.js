3005d7fe98e1f31fc91b78c48b0cbd79dcd9ac5a
Ubuntu <ubuntu@ip-172-31-42-161.us-west-2.compute.internal>
2013-07-23 05:03:20 +0000

    Update web.js and index.html for HW4-p1

--- web.js
+++ web.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = fs.readFileSync('index.html');
-  response.send(buf.toString()); 
-  //response.send('Hello World 2!');
+  var html = fs.readFileSync('index.html').toString();
+  response.send(html); 
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
